4f0ed24ab46a653d7bfd71a0d35573187cf0792d
suprith <supriths4804@gmail.com>
2024-07-27 04:43:44 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import WelcomePage from './WelcomePage';
+import HomePage from './HomePage';
+import AnonymousComplaintPage from './AnonymousComplaint';
+import NonAnonymousComplaintPage from './NonAnonymousComplaint';
+import ConfirmationPage from './ComplaintConfirmation';
+import CheckComplaintStatus from './checkstatus';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/file-anonymous-complaint" element={<AnonymousComplaintPage />} />
+        <Route path="/file-non-anonymous-complaint" element={<NonAnonymousComplaintPage />} />
+        <Route path="/confirmation" element={<ConfirmationPage />} />
+        <Route path="/check-complaint-status" element={<CheckComplaintStatus />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
